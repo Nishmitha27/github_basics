1d51d8b2334930691af829d11527d4ec3cc9a885
nnm23ad032-afk <nishpr2427@gmail.com>
2026-06-05 12:28:36 +0530

    sadasd

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+from flask import Flask
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return "hello world"
+
+@app.route("/abc")
+def abc():
+    return "This is the ABC route"
+
+@app.route("/<name>")
+def greet(name):
+    return f"hello, {name}!"
+
+if __name__=="__main__":
+    app.run(debug=True)
+
